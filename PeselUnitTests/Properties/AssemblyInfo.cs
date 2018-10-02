09e8e4f3addbf343aa6738175032e88aed0b1845
Natasza Płachta <34610856+nataszaplachta@users.noreply.github.com>
2018-10-02 18:42:47 +0200

    creating unit tests

---
+++ PeselUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PeselUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PeselUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1c015ab-be06-4343-bb47-aad14e18b479")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PeselUnitTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeselConsole;
+
+namespace PeselUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LosowyPeselNumerKontrolny()
+        {
+            Random r = new Random();      
+            int a = r.Next(1, 9);
+            int b = r.Next(0, 9);
+            int c = r.Next(0, 9);
+            int d = r.Next(0, 9);
+            int e = r.Next(0, 9);
+            int f = r.Next(0, 9);
+            int g = r.Next(0, 9);
+            int h = r.Next(0, 9);
+            int i = r.Next(0, 9);
+            int j = r.Next(0, 9);
+            int k = r.Next(0, 9);
+
+            string spesel = $"{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}";
+            double pesel = Convert.ToDouble(spesel);
+
+            double x = 9 * a + 7 * b + 3 * c + 1 * d + 9 * e + 7 * f + 3 * g + 1 * h + 9 * i + 7 * j;
+            double expectedKontrolny = Char.GetNumericValue(spesel[10]);
+
+            double kontrolny = x % 10;
+
+            Assert.AreEqual(kontrolny, expectedKontrolny);
+
+        }
+    }
+}
